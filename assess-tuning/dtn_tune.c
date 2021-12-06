8d0ee1677c264baab71c9255555a1bbd22afa71b
David Miranda <david.ma2.miranda@outlook.com>
2021-12-06 02:25:48 -0500

    issue concerning htcp congestion control

--- assess-tuning/dtn_tune.c
+++ assess-tuning/dtn_tune.c
@@ -280,8 +280,9 @@ return;
 #define bbr 		0
 #define fq		 	1
 #define htcp	 	2
-#define getvalue  	3
-char *aStringval[] ={"bbr", "fq", "htcp", "getvalue"};
+#define	reno		3
+#define getvalue	4
+char *aStringval[] ={"bbr", "fq", "htcp", "reno", "getvalue"};
 
 typedef struct {
     char * setting;
@@ -310,8 +311,8 @@ host_tuning_vals_t aTuningNumsToUse[TUNING_NUMS] = {
     {"net.core.rmem_max",   						67108864,          -1,      	0},
     {"net.core.wmem_max",   						67108864,          -1,      	0},
     {"net.ipv4.tcp_mtu_probing",			   			   1,          -1,      	0},
-	{"net.ipv4.tcp_available_congestion_control",   getvalue,          -1,          0},
-    {"net.ipv4.tcp_congestion_control",	    			htcp, 		   -1,			0}, //uses #defines to help
+	{"net.ipv4.tcp_available_congestion_control",	getvalue,   	   -1, 			0},
+    {"net.ipv4.tcp_congestion_control",	    			reno, 		   -1,			0}, //uses #defines to help
     {"net.core.default_qdisc",		          			  fq, 		   -1,			0}, //uses #defines
     {"net.ipv4.tcp_rmem",       						4096,      	87380,   33554432},
     {"net.ipv4.tcp_wmem",       						4096,       65536,   33554432},
@@ -381,7 +382,7 @@ void fDoSystemTuning(void)
 		else
 			setting[len] = 0;
 
-		if (strcmp(setting, "net.ipv4.tcp_available_congestion_control") != 0) //don't print
+		if (strcmp(setting, "net.ipv4.tcp_available_congestion_control") != 0) //print in this case
 			fprintf(tunLogPtr,"%s",setting);
 
 		/* compare with known list now */
@@ -585,23 +586,52 @@ void fDoSystemTuning(void)
 					{ //must be a string
 						if (strcmp(value, aStringval[aTuningNumsToUse[count].minimum]) != 0)
 						{
+#if 1
 							if (strcmp(setting, "net.ipv4.tcp_available_congestion_control") == 0)
                             {
-                                if (strstr(value,"htcp"))
+                                if (strstr(value,"reno"))
                                     congestion_control_recommended_avail = 1;
 
                                 break;
                             }
+#endif
 							
 							fprintf(tunLogPtr,"%*s", vPad, value);	
 
                             if (strcmp(setting, "net.ipv4.tcp_congestion_control") == 0)
                             {
                                 if (!congestion_control_recommended_avail)
-                                {
-                                    fprintf(tunLogPtr,"%26s %20s\n",aStringval[aTuningNumsToUse[count].minimum], "*htcp na");
-                                    break; //skip
-                                }
+								{ //try modprobe
+									char modprobe_str[64];
+									char *line2 = NULL;
+    								size_t len2 = 0;
+    								ssize_t nread2;
+									FILE * modprobeFilePtr = 0;
+
+									sprintf(modprobe_str,"%s","modprobe tcp_reno > /tmp/modprobe_result 2>&1");
+									system(modprobe_str);
+
+									modprobeFilePtr = fopen("/tmp/modprobe_result", "r");	
+									if (!modprobeFilePtr)
+									{
+										int save_errno = errno;
+							       		gettime(&clk, ctime_buf);
+        								fprintf(tunLogPtr,"\n%s %s: Opening of %s failed, errno = %d\n",ctime_buf, phase2str(current_phase), "/tmp/modprobe_result", save_errno);
+                                    	fprintf(tunLogPtr,"%s %s: ***Could not determine what value to set net.ipv4.tcp_congestion_control", ctime_buf, phase2str(current_phase));
+										break;
+									}
+
+									nread2 = getline(&line2, &len2, modprobeFilePtr);
+									printf("*******NNNNNNNNnread2 = %ld len = %ld p = %p, strlen of line %ld\n",nread2,len, line2, strlen(line2));
+									fclose(modprobeFilePtr);
+								
+									if (nread2 != -1)
+                                	{
+                                    	fprintf(tunLogPtr,"%26s %20s\n",aStringval[aTuningNumsToUse[count].minimum], "*reno na");
+                                    	break; //skip
+                                	}
+								
+								}
                             }
 
 							fprintf(tunLogPtr,"%26s %20c\n",aStringval[aTuningNumsToUse[count].minimum], gApplyDefSysTuning);
