0ded07c5a73b84b8fb00f5cf42908d57cecb3ff9
David Miranda <dm33187@hotmail.com>
2022-07-11 18:05:35 +0000

    fix up timer routines

--- userspace/user_dtn.c
+++ userspace/user_dtn.c
@@ -45,6 +45,38 @@ void gettime(time_t *clk, char *ctime_buf)
 	ctime_buf[24] = ':';
 }
 
+static int makeTimer( char *name, timer_t *timerID, int expireMS, int intervalMS )
+{
+	struct sigevent         te;
+	struct itimerspec       its;
+	struct sigaction        sa;
+	int                     sigNo = SIGRTMIN;
+
+	/* Set up signal handler. */
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = timerHandler;
+	sigemptyset(&sa.sa_mask);
+	if (sigaction(sigNo, &sa, NULL) == -1)
+	{
+		fprintf(stderr, "%s: Failed to setup signal handling for %s.\n", PROG, name);
+		return(-1);
+	}
+
+	/* Set and enable alarm */
+	te.sigev_notify = SIGEV_SIGNAL;
+	te.sigev_signo = sigNo;
+	te.sigev_value.sival_ptr = timerID;
+	timer_create(CLOCK_REALTIME, &te, timerID);
+
+	its.it_interval.tv_sec = 0;
+	its.it_interval.tv_nsec = intervalMS * 1000000;
+	its.it_value.tv_sec = 0;
+	its.it_value.tv_nsec = expireMS * 1000000;
+	timer_settime(*timerID, 0, &its, NULL);
+
+	return(0);
+}
+
 /******HTTP server *****/
 #include "fio.h"
 #include "http.h"
