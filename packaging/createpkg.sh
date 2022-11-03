8f9696831f00f3165b3e6ca69edc7e7c2b3598e8
David Miranda <david.ma2.miranda@outlook.com>
2022-11-03 17:13:14 +0000

    creating and installing the package

--- packaging/createpkg.sh
+++ packaging/createpkg.sh
@@ -42,7 +42,7 @@ cp ../util/plotgraph.py .
 cp ../util/conv_csv_to_json.py .
 
 rm -f SAdtntune.zip
-zip SAdtntune.zip tuncli user_dtn help_dtn.sh user_config.txt user_menu.sh gdv_100.sh gdv.sh readme.txt common_irq_affinity.sh set_irq_affinity.sh plotgraph.py conv_csv_to_json.py
+zip SAdtntune.zip tuncli user_dtn help_dtn.sh user_config.txt user_menu.sh gdv_100.sh gdv.sh readme.txt common_irq_affinity.sh set_irq_affinity.sh plotgraph.py conv_csv_to_json.py install.sh
 #
 rm tuncli 
 rm user_dtn 
