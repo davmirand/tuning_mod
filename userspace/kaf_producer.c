86a76bbcc8648fd6e653c155166e1621bc3ea484
David Miranda <david.ma2.miranda@outlook.com>
2024-03-05 17:19:09 -0500

    added make file for kafka, changed some debug output

--- userspace/kaf_producer.c
+++ userspace/kaf_producer.c
@@ -12,34 +12,36 @@ void fUseKafka(void)
 
 	if (gethostname(hostname, sizeof(hostname))) 
 	{
- 		fprintf(stderr, "%% Failed to lookup hostname\n");
- 		exit(1);
+  		fprintf(stderr, "%% Failed to lookup hostname\n");
+  		exit(1);
 	}
 
 	if (rd_kafka_conf_set(conf, "client.id", hostname, errstr, sizeof(errstr)) != RD_KAFKA_CONF_OK) 
 	{
- 		fprintf(stderr, "%% %s\n", errstr);
- 		exit(1);
-	}
-
-	if (rd_kafka_conf_set(conf, "group.id", "foo", errstr, sizeof(errstr)) != RD_KAFKA_CONF_OK) 
-	{
- 		fprintf(stderr, "%% %s\n", errstr);
- 		exit(1);
+  		fprintf(stderr, "%% %s\n", errstr);
+  		exit(1);
 	}
 
 	if (rd_kafka_conf_set(conf, "bootstrap.servers", "host1:9092,host2:9092", errstr, sizeof(errstr)) != RD_KAFKA_CONF_OK) 
 	{
- 		fprintf(stderr, "%% %s\n", errstr);
- 		exit(1);
+  		fprintf(stderr, "%% %s\n", errstr);
+  		exit(1);
 	}
 
-	/* Create Kafka consumer handle */
-	rd_kafka_t *rk;
-	if (!(rk = rd_kafka_new(RD_KAFKA_CONSUMER, conf, errstr, sizeof(errstr)))) 
+	rd_kafka_topic_conf_t *topic_conf = rd_kafka_topic_conf_new();
+
+	if (rd_kafka_topic_conf_set(topic_conf, "acks", "all", errstr, sizeof(errstr)) != RD_KAFKA_CONF_OK) 
 	{
- 		fprintf(stderr, "%% Failed to create new consumer: %s\n", errstr);
- 		exit(1);
+  		fprintf(stderr, "%% %s\n", errstr);
+  		exit(1);
+	}
+
+	/* Create Kafka producer handle */
+	rd_kafka_t *rk;
+	if (!(rk = rd_kafka_new(RD_KAFKA_PRODUCER, conf, errstr, sizeof(errstr)))) 
+	{
+  		fprintf(stderr, "%% Failed to create new producer: %s\n", errstr);
+  		exit(1);
 	}
 
 }
